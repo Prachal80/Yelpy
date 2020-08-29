347b0843a0d7cde47e140a837e729a8aacaa01a7
Prachal80 <prachaljpatel@gmail.com>
2020-08-28 18:35:33 -0700

    First commit

---
+++ Yelpy/Controllers/RestaurantsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class RestaurantsViewController: UIViewController {
+    
+    // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
+    // ––––– TODO: Next, place TableView outlet here
+    
+    
+    // –––––– TODO: Initialize restaurantsArray
+    
+    
+    
+    // ––––– TODO: Add tableView datasource + delegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    
+    // ––––– TODO: Get data from API helper and retrieve restaurants
+    
+
+}
+
+// ––––– TODO: Create tableView Extension and TableView Functionality
+
+
---
+++ Yelpy/Network/API.swift
@@ -0,0 +1,56 @@
+//
+//  File.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import Foundation
+
+
+struct API {
+    
+
+    
+    static func getRestaurants(completion: @escaping ([[String:Any]]?) -> Void) {
+        
+        // ––––– TODO: Add your own API key!
+        let apikey = ""
+        
+        // Coordinates for San Francisco
+        let lat = 37.773972
+        let long = -122.431297
+        
+        
+        let url = URL(string: "https://api.yelp.com/v3/transactions/delivery/search?latitude=\(lat)&longitude=\(long)")!
+        
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        // Insert API Key to request
+        request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                
+        
+
+                // ––––– TODO: Get data from API and return it using completion
+                
+                
+                
+                return completion([[:]])
+                
+                }
+            }
+        
+            task.resume()
+        
+        }
+    }
+
+    
